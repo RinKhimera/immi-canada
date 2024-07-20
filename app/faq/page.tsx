3a76c54528935e54c24adf0d1bb4893a47c23437
Samuel Pokam <dixiades@gmail.com>
2024-07-20 09:06:36 -0400

    Create page.tsx

---
+++ app/faq/page.tsx
@@ -0,0 +1,16 @@
+const FaqPage = () => {
+  return (
+    <main className="min-h-screen xl:px-16 pt-12">
+      <section className="flex flex-col items-center text-center">
+        <h1 className="text-3xl lg:text-4xl max-w-3xl text-center font-medium">
+          Tout ce que vous devez savoir sur les{" "}
+          <span className="text-primary font-bold">sigles </span>
+          et <span className="text-primary font-bold">abréviations </span>
+          en un seul endroit!
+        </h1>
+      </section>
+    </main>
+  )
+}
+
+export default FaqPage
